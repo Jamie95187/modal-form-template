5c2e5c19a4b3e57e6275eb42a0204924b0761d78
Jamie95187 <jamiewong63@googlemail.com>
2020-05-26 14:27:34 +0100

    implement custom TextInput component

---
+++ src/TextInput/TextInput.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './TextInput.css';
+
+class TextInput extends Component {
+  render() {
+    return (
+      <div className="field" />
+    )
+  }
+}
+
+export default TextInput;
