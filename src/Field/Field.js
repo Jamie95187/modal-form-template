4cd924c71354201b7b8524caffb0221ab435cdd7
Jamie95187 <jamiewong63@googlemail.com>
2020-05-26 20:07:32 +0100

    conditionally rendering css on field element

--- src/Field/Field.js
+++ src/Field/Field.js
@@ -19,13 +19,49 @@ class Field extends Component {
     value: '',
     error: '',
     label: '',
+    predicted: '',
     onChange: () => ''
   };
 
-  render() {
-
+  // Updates the changes made in the input to be displayed in the DOM
+  onChange = event => {
+    const { id } = this.props;
+    const value = event.target.value;
+    this.setState({ value, error: '' });
+    return this.props.onChange(id, value);
   }
 
+  constructor(props) {
+
+    super(props);
+
+    // When the 'locked' prop is set to true, we'd like the input only to become focussed
+    // when the 'focussed' prop is set to true.
+
+    this.state = {
+      focussed: (props.locked && props.focussed) || false,
+      value: props.value || '',
+      error: props.error || '',
+      label: props.label || ''
+    };
+  }
+
+  render() {
+    const { focussed, value, error, label } = this.state;
+    const { id, type, locked } = this.props;
+
+    return (
+      <div className="field">
+        <input
+          id={id}
+          type="text"
+          value={value}
+          placeholder={label}
+          onChange={this.onChange}
+        />
+      </div>
+    );
+  }
 }
 
 export default Field;
